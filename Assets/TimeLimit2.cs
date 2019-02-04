351e72fca4fd6befbe9046ebd2b313e2fd67b078
LCH <ckdgus4002@naver.com>
2019-02-04 17:32:19 +0900

    Initial commit

---
+++ Assets/TimeLimit2.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
+public class TimeLimit2 : MonoBehaviour { 
+    float timeRemaining = 1800;
+
+
+    void Update() {
+        timeRemaining -= Time.deltaTime;
+    }
+    void OnGUI() {
+        if(timeRemaining > 1) {
+            GUI.Label(new Rect(100, 100, 200, 100), "허기 : " + (int)timeRemaining);
+        }
+        else {
+			SceneManager.LoadScene (2);
+        }
+    }
+}
---
+++ Assets/TouchRose.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TouchRose : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.transform.tag == "Player")
+			SceneManager.LoadScene (3);
+        if(col.transform.tag == "Zombie")
+			SceneManager.LoadScene (3);
+    }
+}
